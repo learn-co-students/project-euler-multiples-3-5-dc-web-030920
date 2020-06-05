a213014259b204f74908f0091991ee952f6b8ae2
kaidornel <kai_dornel@hotmail.com>
2020-06-05 09:42:45 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,26 @@
 # Enter your procedural solution here!
+
+
+def collect_multiples(limit)
+    i = 1
+    number_collection = []
+    while i < limit do
+        if i % 3 == 0 || i % 5 == 0
+            number_collection.push(i)
+        end
+        i += 1
+    end
+    number_collection
+end
+
+def sum_multiples(limit)
+    i = 1
+    number_collection = []
+    while i < limit do
+        if i % 3 == 0 || i % 5 == 0
+            number_collection.push(i)
+        end
+        i += 1
+    end
+    number_collection.sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+
+    def initialize(limit)
+        @limit = limit
+        # binding.pry
+    end
+
+    def collect_multiples
+        (1...@limit).to_a.reject{|num| num % 3 != 0 && num % 5 != 0}
+    end
+
+    def sum_multiples
+        (1...@limit).to_a.reject{|num| num % 3 != 0 && num % 5 != 0}.sum
+    end
+
+end
